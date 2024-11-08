e2d11df77d9b6ccabe344f707a3aaeba3ed403b2
JeffereyWu <h13jefferey@gmail.com>
2024-11-09 00:41:43 +0800

    new file:   .gitignore 	new file:   README.md 	new file:   components/Main.jsx 	new file:   components/Navbar.jsx 	new file:   eslint.config.js 	new file:   images/react-icon-large.png 	new file:   images/react-icon-small.png 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.css 	new file:   src/App.jsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   src/pizza.png 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from "../components/Main"
+import Main from "../components/Navbar"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Main />
+            <Navbar />
+            
+        </div>
+    )
+}
+
